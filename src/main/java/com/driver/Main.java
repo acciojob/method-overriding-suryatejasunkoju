17db1736fab95fa91357246e3769cf7dad2de95f
suryatejasunkoju <sunkojusuryateja@gmail.com>
2023-01-24 20:08:50 +0530

    again trying

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,3 +5,13 @@ public class Main {
         bObj.meth();
     }
 }
+class A {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class  B extends A{
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
